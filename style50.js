e9f31d45e1abde139710ad4c3c18d2b1ad5d43c4
Will Claybaugh <wclaybaugh@gmail.com>
2017-08-17 21:37:54 +0000

    apply style50 to our own code, because we're not that ironic

--- style50.js
+++ style50.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module) {
     main.consumes = [
-        "dialog.error","Panel","proc","save","settings","tabManager","ui"
+        "dialog.error", "Panel", "proc", "save", "settings", "tabManager", "ui"
     ];
     main.provides = ["harvard.cs50.style"];
     return main;
@@ -43,8 +43,8 @@ define(function(require, exports, module) {
             //match plugin's code display to IDE's code display
             var font_family = settings.get("user/ace/@fontFamily");
             var font_text = settings.get("user/ace/@fontSize") + "px";
-            ui.setStyleRule("#style50","font-family",font_family);
-            ui.setStyleRule("#style50","font-size",font_text);
+            ui.setStyleRule("#style50", "font-family", font_family);
+            ui.setStyleRule("#style50", "font-size", font_text);
             
             /////
             // Set event handlers
@@ -52,37 +52,40 @@ define(function(require, exports, module) {
             
             //all open tabs should talk to us when given focus
             tabManager.once("ready", function() {
-                tabManager.on("focus",function(e){
-                    if (!style50_panel.active){
+                tabManager.on("focus", function(e) {
+                    if (!style50_panel.active) {
                         return;
                     }
-                    emit("draw",{
-                        aml:style50_panel.aml,
-                        html:style50_panel.aml.$int
+                    emit("draw", {
+                        aml: style50_panel.aml,
+                        html: style50_panel.aml.$int
                     });
-                },style50_panel);
+                }, style50_panel);
             });
             
             //when a user saves a file, redraw the style50 pane
-            save.on("afterSave",function(e){
-                emit("draw",{aml:style50_panel.aml,html:style50_panel.aml.$int});
-            },style50_panel);
+            save.on("afterSave", function(e) {
+                emit("draw", {
+                    aml: style50_panel.aml,
+                    html: style50_panel.aml.$int
+                });
+            }, style50_panel);
             
             //update our font size and shape when those settings change
-            settings.on("user/ace/@fontSize",function(new_size){
+            settings.on("user/ace/@fontSize", function(new_size) {
                 var font_text = new_size + "px";
-                ui.setStyleRule("#style50","font-size",font_text);
-            },style50_panel);
+                ui.setStyleRule("#style50", "font-size", font_text);
+            }, style50_panel);
             
-            settings.on("user/ace/@fontFamily",function(new_family_text){
-                ui.setStyleRule("#style50","font-family",new_family_text);
-            },style50_panel);
+            settings.on("user/ace/@fontFamily", function(new_family_text) {
+                ui.setStyleRule("#style50", "font-family", new_family_text);
+            }, style50_panel);
         }
         
         /***** Methods and Helper Functions*****/
         
         //helper to run the style50 command line tool and render output
-        draw_pane = function(e,filepath){
+        draw_pane = function(e, filepath) {
             //clear the window
             e.html.innerHTML = "";
             
@@ -91,30 +94,30 @@ define(function(require, exports, module) {
                 "style50",
                 { args: ["-o","json", filepath] },  //runs in /var/c9sdk
                 function(err, process) {
-                    if (err){
+                    if (err) {
                         grave_error(e, CRASH_ERR_MSG);
                         throw err;
                     }
                     
-                    var error_accumulation=[];
-                    var out_accumulation=[];
+                    var error_accumulation = [];
+                    var out_accumulation = [];
                     
                     // accumulate responses utill the stream ends
                     process.stdout.on("data", function(chunk) {
                         out_accumulation.push(chunk);
                     });
                     
-                    process.stderr.on("data",function(chunk){
+                    process.stderr.on("data", function(chunk) {
                         error_accumulation.push(chunk);
                     });
                     
                     // if the CL tool errors
-                    process.stderr.on("end",function(chunk){
+                    process.stderr.on("end", function() {
                         
                         //stitch together the error and display it
-                        var full_error=error_accumulation.join('');
+                        var full_error = error_accumulation.join('');
                         
-                        if (full_error){
+                        if (full_error) {
                             grave_error(e, RUNTIME_ERR_MSG);
                             console.log("error in style50:");
                             console.log(full_error);
@@ -123,41 +126,41 @@ define(function(require, exports, module) {
                     });
                     
                     // if the CL tool works
-                    process.stdout.on("end", function(chunk) {
+                    process.stdout.on("end", function() {
                         
                         //stitch the various chunks together
-                        var full_output=out_accumulation.join('');
+                        var full_output = out_accumulation.join('');
                         
-                        if (!full_output){
+                        if (!full_output) {
                             grave_error(e, NO_RESULT_MSG);
                             return;
                         }
                         
                         // bad output
                         var style50_dict = JSON.parse(full_output);
-                        if (!style50_dict[filepath]){
+                        if (!style50_dict[filepath]) {
                             grave_error(e, BAD_RESULT_MSG);
                             return;
                         }
                         
                         // handled internal error (e.g. couldn't find file)
                         var error_msg = style50_dict[filepath].error;
-                        if (error_msg){
-                            html_error(e,"Style50: " + error_msg);
+                        if (error_msg) {
+                            html_error(e, "Style50: " + error_msg);
                             return;
                         }
                         
                         // missing fields
                         var diff_html = style50_dict[filepath].diff;
                         var percent_score = style50_dict[filepath].score;
-                        if (!diff_html || ! percent_score){
+                        if (!diff_html || !percent_score) {
                             html_error(e, BAD_RESULT_MSG);
                         }
                         
                         // process results
                         
                         // if code style is perfect, congratualte the user and quit
-                        if (percent_score === 1){
+                        if (percent_score === 1) {
                             e.html.innerHTML = "<div id = 'style50_perfect'>" + "<br>" + "Your code is styled beautifully!!" + "</div>";
                             return;
                         }
@@ -166,9 +169,9 @@ define(function(require, exports, module) {
                         var line_array = diff_html.split("\n");
                         var nonempty_insert = /<ins>.+<\/ins>/;
                         var nonempty_delete = /<del>.+<\/del>/;
-                        var bad_line_count = line_array.reduce(function(count,cur_line){
+                        var bad_line_count = line_array.reduce(function(count, cur_line) {
                             return count + (nonempty_insert.test(cur_line) || nonempty_delete.test(cur_line));
-                        },0);
+                        }, 0);
                         
                         score_html = "<div>You have " + bad_line_count + " lines styled incorrectly";
                         
@@ -179,15 +182,15 @@ define(function(require, exports, module) {
         };
         
         //helper to diplay an error message in the panel
-        function html_error(e, message){
+        function html_error(e, message) {
             e.html.innerHTML = "<div id = 'style50_error'>" + message + "</div>";
         }
         
         //helper to diplay a (serious) error message as a pop-in
-        function grave_error(e, message){
+        function grave_error(e, message) {
             var showError = imports["dialog.error"].show;
             html_error(e, message);
-            return showError(message,3000);
+            return showError(message, 3000);
         }
         
         /***** Lifecycle *****/
@@ -201,9 +204,9 @@ define(function(require, exports, module) {
         });
         
         style50_panel.on("show", function() {
-            emit("draw",{
-                aml:style50_panel.aml,
-                html:style50_panel.aml.$int
+            emit("draw", {
+                aml: style50_panel.aml,
+                html: style50_panel.aml.$int
             });
         });
         
@@ -215,20 +218,20 @@ define(function(require, exports, module) {
             var cur_tab = tabManager.focussedTab;
             
             //if no tabs are open
-            if (!cur_tab){
+            if (!cur_tab) {
                 return;
             }
             
             //if the tab is a terminal, preferences page, or other item without a filepath
             var filepath = cur_tab.path;
-            if (!filepath){
+            if (!filepath) {
                 return;
             }
             
             var fullpath;
-            if (filepath[0]==='/'){
+            if (filepath[0] === '/') {
                 fullpath = path.join("/home/ubuntu/workspace", filepath);
-            }else{
+            } else {
                 fullpath = path.join("/home/ubuntu/", filepath.substring(2));
             }
             
@@ -238,7 +241,7 @@ define(function(require, exports, module) {
                 return;
             }
             
-            draw_pane(e,fullpath);
+            draw_pane(e, fullpath);
         });
         
         /***** Register and define API *****/
